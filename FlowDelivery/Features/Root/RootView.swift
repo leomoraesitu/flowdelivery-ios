cb94899cff25bee6543afbceded4d524972d66c0
Léo Moraes <leomoraesitu@hotmail.com>
2026-07-15 14:56:32 -0300

    feat(app): introduce RootView as application entry

---
+++ FlowDelivery/Features/Root/RootView.swift
@@ -0,0 +1,18 @@
+//
+//  RootView.swift
+//  FlowDelivery
+//
+//  Created by Leonardo de Moraes Souza on 15/07/26.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RootView()
+}
