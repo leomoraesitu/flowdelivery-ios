de884ae32a3be8631fa13a9ea9f203290e866a02
Léo Moraes <leomoraesitu@hotmail.com>
2026-07-22 16:18:33 -0300

    feat(di): register auth service in app container

--- FlowDelivery/Core/DI/AppContainer.swift
+++ FlowDelivery/Core/DI/AppContainer.swift
@@ -3,14 +3,16 @@ import Observation
 @Observable
 final class AppContainer {
     let sessionStore: SessionStore
+    let authService: AuthService
 
     let rootViewModel: RootViewModel
 
     init() {
         let sessionStore = SessionStore()
+        let authService = AuthService()
 
         self.sessionStore = sessionStore
-
+        self.authService = authService
         rootViewModel = RootViewModel(
             sessionStore: sessionStore
         )
