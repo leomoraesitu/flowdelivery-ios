64b9a11a6edc875928dd7298ff99db628efd1f9b
Léo Moraes <leomoraesitu@hotmail.com>
2026-07-22 20:53:44 -0300

    feat(auth): introduce authentication repository (#21)

--- FlowDelivery/Core/Services/AuthService.swift
+++ FlowDelivery/Core/Services/AuthService.swift
@@ -2,16 +2,29 @@ import Foundation
 
 final class AuthService {
     private let sessionStore: SessionStore
+    private let repository: AuthRepository
 
-    init(sessionStore: SessionStore) {
+    init(
+        repository: AuthRepository,
+        sessionStore: SessionStore
+    ) {
+        self.repository = repository
         self.sessionStore = sessionStore
     }
 
     func login() {
+        let authenticated = repository.login()
+
+        guard authenticated else {
+            return
+        }
+
         sessionStore.login()
     }
 
     func logout() {
+        repository.logout()
+
         sessionStore.logout()
     }
 }
