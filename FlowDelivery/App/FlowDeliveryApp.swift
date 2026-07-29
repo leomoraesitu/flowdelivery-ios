d7d884238dce3109fa68c0d2a411b006acad40bb
Léo Moraes <leomoraesitu@hotmail.com>
2026-07-29 15:36:03 -0300

    feat(startup): introduce app startup view model

--- FlowDelivery/App/FlowDeliveryApp.swift
+++ FlowDelivery/App/FlowDeliveryApp.swift
@@ -1,29 +1,37 @@
-//
-//  FlowDeliveryApp.swift
-//  FlowDelivery
-//
-//  Created by Leonardo de Moraes Souza on 14/07/26.
-//
-
 import SwiftUI
 
 @main
 struct FlowDeliveryApp: App {
     @State
-    private var container = AppContainer()
+    private var container: AppContainer
+
+    @State
+    private var startupViewModel: AppStartupViewModel
+
+    init() {
+        let container = AppContainer()
+
+        _container = State(
+            initialValue: container
+        )
+
+        _startupViewModel = State(
+            initialValue: AppStartupViewModel(
+                container: container
+            )
+        )
+    }
 
     var body: some Scene {
         WindowGroup {
-            RootView(viewModel: container.rootViewModel)
-                .environment(container)
-                .environment(container.sessionStore)
-                .task {
-                    do {
-                        try container.restoreSession()
-                    } catch {
-                        // Tratamento temporário até a criação do AppStartupViewModel.
-                    }
-                }
+            RootView(
+                viewModel: container.rootViewModel
+            )
+            .environment(container)
+            .environment(container.sessionStore)
+            .task {
+                startupViewModel.start()
+            }
         }
     }
 }
