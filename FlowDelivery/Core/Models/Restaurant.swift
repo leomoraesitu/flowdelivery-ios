b7345cc513a9b3de66ed19e68ab651c9545b19e9
Léo Moraes <leomoraesitu@hotmail.com>
2026-08-03 13:52:46 -0300

    feat(home): add restaurant image support

--- FlowDelivery/Core/Models/Restaurant.swift
+++ FlowDelivery/Core/Models/Restaurant.swift
@@ -3,6 +3,7 @@ import Foundation
 struct Restaurant: Identifiable, Equatable {
     let id: UUID
     let name: String
+    let imageURL: URL?
     let rating: Double
     let deliveryTime: Int
     let deliveryFee: Decimal
