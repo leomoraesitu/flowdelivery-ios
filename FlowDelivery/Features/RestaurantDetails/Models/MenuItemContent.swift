066ed8e35595b38da1736a2760837472e46efe2b
Léo Moraes <leomoraesitu@hotmail.com>
2026-08-05 00:13:37 -0300

    chore(tooling): configure project quality tools (#60)

--- FlowDelivery/Features/RestaurantDetails/Models/MenuItemContent.swift
+++ FlowDelivery/Features/RestaurantDetails/Models/MenuItemContent.swift
@@ -2,13 +2,9 @@ import Foundation
 
 struct MenuItemContent: Identifiable, Equatable {
     let id: UUID
-
     let title: String
-
     let description: String
-
     let price: String
-
     let imageURL: URL?
 }
 
@@ -17,15 +13,11 @@ extension MenuItemContent {
         menuItem: MenuItem
     ) {
         id = menuItem.id
-
         title = menuItem.name
-
         description = menuItem.description
-
         price = menuItem.price.formatted(
             .currency(code: "BRL")
         )
-
         imageURL = menuItem.imageURL
     }
 }
