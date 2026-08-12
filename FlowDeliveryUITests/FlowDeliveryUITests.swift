34b6b42b08f3ba3064e8ed279e434096ce719bdc
Léo Moraes <leomoraesitu@hotmail.com>
2026-08-12 14:08:33 -0300

    test(ui): add home smoke test

--- FlowDeliveryUITests/FlowDeliveryUITests.swift
+++ FlowDeliveryUITests/FlowDeliveryUITests.swift
@@ -1,43 +1,28 @@
-//
-//  FlowDeliveryUITests.swift
-//  FlowDeliveryUITests
-//
-//  Created by Leonardo de Moraes Souza on 14/07/26.
-//
-
 import XCTest
 
 final class FlowDeliveryUITests: XCTestCase {
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests
-        // before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     @MainActor
-    func testExample() {
-        // UI tests must launch the application that they test.
+    func testHomeScreenLoadsRestaurants() {
         let app = XCUIApplication()
+
         app.launch()
 
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // XCUIAutomation Documentation
-        // https://developer.apple.com/documentation/xcuiautomation
-    }
+        let loginButton = app.buttons["Entrar"]
+        XCTAssertTrue(
+            loginButton.waitForExistence(timeout: 5)
+        )
+        loginButton.tap()
 
-    @MainActor
-    func testLaunchPerformance() {
-        // This measures how long it takes to launch your application.
-        measure(metrics: [XCTApplicationLaunchMetric()]) {
-            XCUIApplication().launch()
-        }
+        XCTAssertTrue(
+            app.navigationBars["FlowDelivery"].waitForExistence(
+                timeout: 5
+            )
+        )
+
+        XCTAssertTrue(
+            app.staticTexts["Pizzaria Itália"].waitForExistence(
+                timeout: 5
+            )
+        )
     }
 }
